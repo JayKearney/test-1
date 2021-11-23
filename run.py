267ca332f7d0a8f92d82d72dd37700733cb113e0
JayKearney <indi.mh@gmail.com>
2021-11-23 20:16:00 +0000

    start of game

--- run.py
+++ run.py
@@ -21,22 +21,56 @@ def player_input():
     return (value)
 
 def goes_first():
-    print(["Player","Computer"][randint(0,1)])
+    if random.randint(0,1) == 0:
+        return "Computer"
+    else:
+        return "Player"
+
+def empty_check(board, position):
+    """Check if a space is empty
+            board(list)
+            position(int): Between 1 and 9
+    """
+    return board[position] == " "
+
+def user_choice(board):
+    """Player to choose position. Must be in range of 1-9 where a space is empty
+            return(int): Valid position number
+    """
+    position = 0
+    while position not in [1,2,3,4,5,6,7,8,9] or not empty_check(board, position):
+        position = int(input('Choose a position (1-9): '))
+    return position
+
+def place_marker(board, marker, position):
+    """Place the marker of the player in the corresponding position on the board
+            board(list)
+            marker(string): Either 'X' or 'O'
+            position(integer): Between 1 and 9
+    """
+    #loop through dictionary 
+    
+    board[position] = marker
     
 def main():
     print("Tic Tac Toe")
 
     person, computer = '',''
 
-#printBoard(theBoard)
-
     while True:
         # Set up the board
-        Board = { 'top-left': ' ', 'top-center': ' ', 'top-rigt': ' ',
-        'mid-left': ' ', 'mid-center': ' ', 'mid-right': ' ',
-         'lower-left': ' ', 'lower-center': ' ', 'lower-right': ' '} 
+        board = { 'top-left': ' ', 'top-center': ' ', 'top-rigt': ' ',
+                 'mid-left': ' ', 'mid-center': ' ', 'mid-right': ' ',
+                 'lower-left': ' ', 'lower-center': ' ', 'lower-right': ' '} 
         player = player_input()
 
         # Determine who will go first
         turn = goes_first()
         print(turn + " will go first")
+
+        while playing:
+            if turn == "Player":
+                printBoard(board)
+                position = user_choice(board)
+
+
